b10e7496e9477dff10f28614275842671d629874
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-01-31 12:51:45 +0530

    command handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, Collection } = require("discord.js");
 require("dotenv").config();
+const fs = require("fs");
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -9,24 +10,38 @@ const client = new Client({
   ],
 });
 
+client.commands = new Collection();
+
 const PREFIX = ".";
 
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require("./commands/${file}");
+  client.commands.set(command.name, command);
+}
+
 client.once("ready", () => {
-  console.log("Bot is online!");
+  console.log("Bot is online!🤖");
 });
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
   if (message.content.indexOf(PREFIX) !== 0) return;
 
-  if (message.content === ".talk") {
-    message.reply({ content: "padhai karo bc" });
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (!client.commands.has(command)) return;
+  try {
+    client.commands.get(command).execute(client, message, args);
+  } catch (error) {
+    message.reply(
+      `There was an error trying to execute that command! ${error}`
+    );
+    console.error(error);
   }
-  if (message.content.startsWith(".say")) {
-    const args = message.content.slice(5).split(" ");
-    const sayMessage = args.join(" ");
-    message.channel.send(sayMessage);
-  }
-});
 
-client.login(process.env.TOKEN.toString());
+  client.login(process.env.TOKEN.toString());
+});
